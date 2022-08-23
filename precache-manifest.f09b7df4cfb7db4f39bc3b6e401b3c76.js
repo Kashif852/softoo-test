7506a89ea8249f8f9a8ba210a60ec6fa9896615d
kashifhussain <kashifhussaindahri123@gmail.com>
2022-08-23 11:59:40 -0700

    Updates

---
+++ precache-manifest.f09b7df4cfb7db4f39bc3b6e401b3c76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17f0aa1e6ffc582981dfbd29c64b8412",
+    "url": "/softoo-test/index.html"
+  },
+  {
+    "revision": "6cf90b1ce0ccb6a3eb44",
+    "url": "/softoo-test/static/css/main.236ef789.chunk.css"
+  },
+  {
+    "revision": "8686f0e40b2d9bae8d55",
+    "url": "/softoo-test/static/js/2.8ecc9f83.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/softoo-test/static/js/2.8ecc9f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6cf90b1ce0ccb6a3eb44",
+    "url": "/softoo-test/static/js/main.eaa222a0.chunk.js"
+  },
+  {
+    "revision": "9aae181827ed585ae288",
+    "url": "/softoo-test/static/js/runtime-main.b2716c69.js"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/softoo-test/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "/softoo-test/static/media/shopping-cart-solid.8bf1af37.svg"
+  },
+  {
+    "revision": "2c5552a10c3ab0a115c9484f908f982d",
+    "url": "/softoo-test/static/media/times-solid.2c5552a1.svg"
+  }
+]);
